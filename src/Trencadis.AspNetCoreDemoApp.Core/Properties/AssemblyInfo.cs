46999722368e196bfbf7481163d9d00d0ea6f799
Trencadis-Labs-Admin <florin@trencadis.ro>
2016-12-21 10:59:58 +0200

    Standard Trencadis project structure setup

---
+++ src/Trencadis.AspNetCoreDemoApp.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Trencadis.AspNetCoreDemoApp.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59c1f1e0-c33d-4ebc-8d23-43d308a3156e")]
