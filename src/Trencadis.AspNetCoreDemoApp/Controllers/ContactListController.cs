55fb4d0268b587464541d5147f87d8f1e9823fb7
Trencadis-Labs <florin@trencadis.ro>
2016-12-22 13:55:04 +0200

    Implementation:: grid with contact list

--- src/Trencadis.AspNetCoreDemoApp/Controllers/ContactListController.cs
+++ src/Trencadis.AspNetCoreDemoApp/Controllers/ContactListController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Trencadis.AspNetCoreDemoApp.DataAccess.Repository;
 
 namespace Trencadis.AspNetCoreDemoApp.Controllers
 {
   public class ContactListController : Controller
   {
+    private readonly IContactListRepository contactListRepository;
+
+    public ContactListController(IContactListRepository contactListRepository)
+    {
+      if (contactListRepository == null)
+      {
+        throw new ArgumentNullException(nameof(contactListRepository));
+      }
+
+      this.contactListRepository = contactListRepository;
+    }
+
     // GET: /<controller>/
     public IActionResult Index()
     {
-      return View();
+      var contactListEntries = this.contactListRepository.GetContactList();
+
+      return View(contactListEntries);
     }
   }
 }
