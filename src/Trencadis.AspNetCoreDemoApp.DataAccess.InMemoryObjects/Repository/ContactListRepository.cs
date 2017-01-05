c7154c5afc7d46990ac83199d1740f50976022d2
Trencadis-Labs-Admin <florin@trencadis.ro>
2017-01-05 10:21:57 +0200

    Contact list controller, repo, models

---
+++ src/Trencadis.AspNetCoreDemoApp.DataAccess.InMemoryObjects/Repository/ContactListRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Trencadis.AspNetCoreDemoApp.Core.POCO;
+using Trencadis.AspNetCoreDemoApp.DataAccess.Repository;
+
+namespace Trencadis.AspNetCoreDemoApp.DataAccess.InMemoryObjects.Repository
+{
+  public class ContactListRepository : IContactListRepository
+  {
+    private readonly InMemoryDatabaseObject database;
+
+    public ContactListRepository(InMemoryDatabaseObject db)
+    {
+      if (db == null)
+      {
+        throw new ArgumentNullException(nameof(db));
+      }
+
+      this.database = db;
+    }
+
+    public IEnumerable<ContactListEntry> GetContactList()
+    {
+      return database.ContactList;
+    }
+  }
+}
